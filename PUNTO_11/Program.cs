bbb0c0ce2370ec75e3f058019616e155cb7b571b
Sebastian Monroy <smonroy461@gmail.com>
2024-09-13 22:10:00 -0500

    tercera parte

--- PUNTO_11/Program.cs
+++ PUNTO_11/Program.cs
@@ -10,7 +10,7 @@ internal class Program
         double bnextra = 100000;
         double objetivo = 1000000;
         double PRcomision = 10;
-        double VN1, VN2, VN3;
+        double VN1, VN2, VN3, EXCELENTE;
 
         Console.Write("Ingresar la primera venta del mes: ");
         VN1=Convert.ToDouble(Console.ReadLine());
@@ -26,14 +26,57 @@ internal class Program
         double comision1= VN1 * PRcomision;
         double comision2= VN2 * PRcomision;
         double comision3= VN3 * PRcomision;
-        
+
         //TOTAL DE COMISIONES
 
-        double total= comision1 + comision2 + comision3 + bnextra + sueldobase;
+        double totalcm= comision1 + comision2 + comision3;
 
+        // Cálculo de la venta que generó mayor comisión
 
-        //OBJETIVO LOGRADO
+        double mayorComision = comision1;
+        int comisionmayor = 1;
 
+        if (comision2 > mayorComision);
+        {
+            mayorComision = comision2;
+            comisionmayor = 2;
+        }
+
+        if (comision3 > mayorComision);
+        {
+            mayorComision = comision3;
+            comisionmayor = 3;
+        }
+
+        // Cálculo del promedio de las comisiones
+        double PromedioComisiones = totalcm / 3;
+
+        // Total recibido (sueldo base + comisiones)
+        double TotalMensual = sueldobase + totalcm;
+
+        // Comprobamos si superó el objetivo y se lleva el beneficio extra
+        double totalVentas = VN1 + VN2 + VN3;
+
+        if (totalVentas >= objetivo)
+        {
+            TotalMensual += bnextra;
+            Console.WriteLine("¡Felicidades! El vendedor superó el objetivo y ha ganado una bonificación de $100.000.");
+        }
+        else
+        {
+            Console.WriteLine("El vendedor no ha superado el objetivo del mes.");
+        }
+
+        // Resultados
+        Console.WriteLine($"\nComisiones por cada venta:");
+        Console.WriteLine($"Comisión por venta 1: ${comision1}");
+        Console.WriteLine($"Comisión por venta 2: ${comision2}");
+        Console.WriteLine($"Comisión por venta 3: ${comision3}");
+
+        Console.WriteLine($"\nTotal de comisiones: ${totalcm}");
+        Console.WriteLine($"Total mensual (sueldo base + comisiones): ${TotalMensual}");
+        Console.WriteLine($"La venta que generó mayor comisión fue la venta {comisionmayor} con ${mayorComision}");
+        Console.WriteLine($"Promedio de comisiones por cada venta: ${PromedioComisiones}");
 
 
 
