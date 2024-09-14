ad21215e91b774f50a288a21d151abec767227df
Sebastian Monroy <smonroy461@gmail.com>
2024-09-13 20:46:26 -0500

    primera parte

--- PUNTO_11/Program.cs
+++ PUNTO_11/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        //DECLARAR CONSTANTE
+        const decimal sueldobase = 2500000;
+        const decimal bnextra = 100000;
+        const decimal objetivo = 1000000;
+        const decimal comision = 10m;
+        double VN1, VN2, VN3;
+
+        Console.Write("Ingresar el total de las ventas en el mes: ")
+    }
+}
