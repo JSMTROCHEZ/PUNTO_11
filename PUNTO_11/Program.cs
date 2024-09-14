7f179be99e619a2c8d96a3a130ee3d9cff96043b
Sebastian Monroy <smonroy461@gmail.com>
2024-09-13 21:44:03 -0500

    segunda parte

--- PUNTO_11/Program.cs
+++ PUNTO_11/Program.cs
@@ -1,15 +1,46 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         //DECLARAR CONSTANTE
-        const decimal sueldobase = 2500000;
-        const decimal bnextra = 100000;
-        const decimal objetivo = 1000000;
-        const decimal comision = 10m;
+        double sueldobase = 2500000;
+        double bnextra = 100000;
+        double objetivo = 1000000;
+        double PRcomision = 10;
         double VN1, VN2, VN3;
 
-        Console.Write("Ingresar el total de las ventas en el mes: ")
+        Console.Write("Ingresar la primera venta del mes: ");
+        VN1=Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingresar la segunda venta del mes: ");
+        VN2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingresar la tercera venta del mes: ");
+        VN3= Convert.ToDouble(Console.ReadLine());
+
+        //COMISIONES
+
+        double comision1= VN1 * PRcomision;
+        double comision2= VN2 * PRcomision;
+        double comision3= VN3 * PRcomision;
+        
+        //TOTAL DE COMISIONES
+
+        double total= comision1 + comision2 + comision3 + bnextra + sueldobase;
+
+
+        //OBJETIVO LOGRADO
+
+
+
+
+
+
+
+
+
     }
 }
